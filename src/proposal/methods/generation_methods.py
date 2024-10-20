735d71ce6429366d9b1d82d25f277ca2e5a90a27
jwywoo <jwywoo26@gmail.com>
2024-10-20 17:03:41 +0900

    [FEAT] #5-1 MVP Feedback Generator
    - Need to apply ResponseDto

---
+++ src/proposal/methods/generation_methods.py
@@ -0,0 +1,29 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+def volume_feedback():
+  print("""
+    마! 기획이 장난이가!
+    양이 이게 뭐꼬?
+    좀 더 채워와라 임마!
+  """)
+  return None
+
+def content_feedback(proposal, content_feedback_prompt):
+  llm = ChatOpenAI(model='gpt-4o-mini', temperature=1)
+  prompt_template = ChatPromptTemplate.from_messages(
+    [("system", content_feedback_prompt), ("user", "{proposal}")]
+  )
+  chain = prompt_template | llm | StrOutputParser()
+  print(chain.invoke({"proposal": proposal}))
+  return None
+
+def regulation_feedback(proposal, regulation_feedback_prompt):
+  llm = ChatOpenAI(model='gpt-4o-mini', temperature=1)
+  prompt_template = ChatPromptTemplate.from_messages(
+    [("system", regulation_feedback_prompt), ("user", "{proposal}")]
+  )
+  chain = prompt_template | llm | StrOutputParser()
+  print(chain.invoke({"proposal": proposal}))
+  return None
