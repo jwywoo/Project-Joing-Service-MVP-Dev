5a541372644320cace4d6153f4fa10b39374018c
jwywoo <jwywoo26@gmail.com>
2024-10-28 21:31:49 +0900

    [FEAT] #13-3 MVP Proposal Eval Optimization Done

--- src/proposal/methods/generation_methods.py
+++ src/proposal/methods/generation_methods.py
@@ -4,19 +4,9 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-from proposal.schemas import ProposalEvaluationResponseDto
 
 # Global VAR
 PROPOSAL = "{proposal}"
-
-def volume_feedback():
-  response_dto = ProposalEvaluationResponseDto(
-    feedback_type=0,
-    feedback="",
-    current_proposal_score=0,
-    regulation_category=[]
-  )
-  return response_dto
 
 def content_feedback(proposal, content_feedback_prompt):
   llm = ChatOpenAI(model='gpt-4o-mini', temperature=1)
@@ -40,5 +30,4 @@ def summary_generator(proposal, summary_generation_prompt):
     [("system", summary_generation_prompt), ("user", PROPOSAL)]
   )
   chain = prompt_template | llm | StrOutputParser()
-  print(chain.invoke({"proposal": proposal}))
-  return None
+  return json.loads(chain.invoke({"proposal": proposal}))
