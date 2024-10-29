4941a4f0eb2f0a5571cecac5495d8ddf585d1a83
jwywoo <jwywoo26@gmail.com>
2024-10-29 23:02:01 +0900

    [FEAT] #15-1 Profile Evaluation Modified
    - Prompt for evaluation
    - ResponseDto

--- src/profile/methods/evaluation_methods.py
+++ src/profile/methods/evaluation_methods.py
@@ -1,3 +1,5 @@
+import json 
+
 from openai import OpenAI
 
 from profile.methods.screenshot_methods import selenium_screenshot
@@ -12,7 +14,15 @@ def screenshot_evaluation(url, prompt):
             "role": "user",
             "content": [
                 {
-                    "type": "text", "text": "Classify whether this youtube channel is appropriate for teenagers or not"
+                    "type": "text", "text": """"You are provided with a screenshot of a YouTube channel page. 
+                    Please analyze only the visible content in the image (such as titles, thumbnails, text, or other visible elements) to classify if the channel content is appropriate based on general content standards.
+                    If any inappropriate elements appear (such as explicit language, mature themes, or unsuitable visuals), specify the reason in Korean in a single sentence. If it’s suitable, return an empty reason field.
+                    Generate the result in the following format:
+                        {
+                            "appropriate": true if there is no explicit or unsuitable content, false otherwise,
+                            "reason": put empty string if it's true and false otherwise.
+                        }
+                        """
                     },
                 {
                     "type": "image_url",
